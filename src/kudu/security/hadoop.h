4e6a3441358718b1d171e4a7a99dc1951320a946
Jeffrey Smith <jeffrey.smith@acceldata.io>
2025-07-11 08:34:23 -0400

    ODP-4302: Add Hadoop auth to local support to Kudu (#10)
    * ODP-4302: Initial auth_to_local for hadoop auth
    
    * ODP-4302: fix variable name
    
    * ODP-4302: Add getting default realm
    
    * ODP-4302: set defaultRealm to empty string
    
    * ODP-4302: Removed boost::regex, fixed structure
    
    * ODP-4302: use logging
    
    * ODP-4302: Cleaned up functions, removed out parameter
    
    * ODP-4302: Use correct variable
    
    * ODP-4302: Additional small fixes
    
    * ODP-4302: security checks, optimizations
    
    * ODP-4302: Remove std::filesystem
    
    * ODP-4302: Fix off by one error
    
    * ODP-4302: Add initial tests
    
    * ODP-4302: Add hadoop_test to list of tests
    
    * ODP-4302: Fix sed flag handling
    
    * ODP-4302: Fix linking issue for test
    
    * ODP-4302: Added tests, fixed bugs exposed by tests
    
    * ODP-4302: Added lots of tests, fixed many edgecases
    
    * ODP-4302: More test cases, formatting, fixes
    
    * ODP-4302: Many changes to code and tests
    
    * ODP-4302: Compatibility fixes to match hadoop's behaviour
    
    * ODP-4302: Switched to string_view where possible
    
    * ODP-4302: Add krb5 header explicitly
    
    * ODP-4302: Cleaned up some code and formatting
    
    * ODP-4302: Added comments, regex safety, simplified things

---
+++ src/kudu/security/hadoop.h
@@ -0,0 +1,137 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#pragma once
+#include <gtest/gtest_prod.h>
+#include <krb5/krb5.h>
+#include <optional>
+#include <regex>
+#include <shared_mutex>
+#include <string>
+#include <vector>
+
+namespace kudu{
+namespace security {
+class HadoopAuthToLocal {
+
+  static constexpr std::size_t kParseFields = 4;
+  static constexpr std::size_t kAtPosDefault = -2;
+
+  enum class RuleMechanism {HADOOP, MIT};
+
+  struct SedRule {
+    std::string pattern;
+    std::string replacement;
+    std::string flags;
+    std::regex compiled_pattern;
+  };
+  struct Rule {
+    int numberOfFields;
+    std::string fmt;
+    std::string rule;
+    std::string regexMatchString;
+    std::regex regexMatch;
+    std::optional<SedRule> sedRule;
+  };
+  struct Token {
+    enum class Type {placeholder, literal};
+    Type type;
+    std::string text;
+  };
+  using Token = HadoopAuthToLocal::Token;
+
+  std::vector<std::string> coreSiteRules_;
+  std::string defaultRealm_ ;
+  std::vector<Rule> rules_;
+  RuleMechanism ruleMechanism_ = RuleMechanism::HADOOP;
+
+  mutable std::shared_mutex mutex_;
+
+  void setDefaultRealm(const std::string& realm);
+
+  static bool checkPrincipal(std::string_view principal, size_t at_pos = kAtPosDefault);
+  static int numberOfFields(std::string_view principal);
+  static std::optional<Rule> initRule(const std::string& auth_rule);
+  static std::optional<bool> match_regex(const std::regex& reg, const std::regex& sed_reg, std::string_view match_string, int milliseconds = 100);
+  static std::optional<SedRule> parseSedRule(std::string_view sed_rule) noexcept;
+  static std::optional<std::array<std::string, kParseFields>> parseAuthToLocalRule(
+    std::string_view rule);
+
+  static std::optional<std::string> format(
+    const std::string& fmt, 
+    const std::vector<std::string>& values);
+
+  static std::optional<std::string> processJavaRegexLiterals(std::string_view input);
+  static std::optional<std::string> replaceMatchingPrincipal(
+    const Rule& rule, 
+    const std::string& formatted_principal);
+
+  static std::optional<std::vector<Token>> tokenize(const std::string& fmt);
+  static std::string escapeJavaRegexLiteral(std::string_view input);
+  static std::string getRealm(std::string_view principal, size_t at_pos = kAtPosDefault);
+  static std::vector<std::string> extractFields(std::string_view principal);
+  
+  bool loadConf(const std::string& filepath);
+  bool matchNumberOfFields(const Rule &rule, std::string_view principal) const;
+  bool setKrb5Context(krb5_context& ctx);
+  bool setRules(std::istream& input);
+  bool simplePatternCheck(std::string_view short_name) const;
+
+  int fieldsMatch(const Rule &rule, std::string_view principal);
+
+  std::optional<std::string> createFormattedPrincipal(
+    const Rule& rule, 
+    const std::vector<std::string>& principal_fields ) const;
+
+  std::optional<std::string> defaultRule(
+    const Rule& rule, 
+    const std::string& principal, 
+    std::string_view realm) const;
+
+  std::optional<std::string> transformPrincipal(const Rule& rule, std::string_view principal) const;
+
+
+  FRIEND_TEST(HadoopAuthToLocalTest, badFormatTest);
+  FRIEND_TEST(HadoopAuthToLocalTest, badInitRulesTest);
+  FRIEND_TEST(HadoopAuthToLocalTest, badLoadRulesTest);
+  FRIEND_TEST(HadoopAuthToLocalTest, badParseAuthToLocalRuleTest);
+  FRIEND_TEST(HadoopAuthToLocalTest, badSedRuleTest);
+  FRIEND_TEST(HadoopAuthToLocalTest, checkPrincipalTest);
+  FRIEND_TEST(HadoopAuthToLocalTest, formatTest);
+  FRIEND_TEST(HadoopAuthToLocalTest, getRealmTest);
+  FRIEND_TEST(HadoopAuthToLocalTest, initRuleTest);
+  FRIEND_TEST(HadoopAuthToLocalTest, loadRulesTest);
+  FRIEND_TEST(HadoopAuthToLocalTest, matchPrincipalAgainstAllRulesTest);
+  FRIEND_TEST(HadoopAuthToLocalTest, numberOfFieldsTest);
+  FRIEND_TEST(HadoopAuthToLocalTest, parseAuthToLocalRuleTest);
+  FRIEND_TEST(HadoopAuthToLocalTest, ruleMechanismTest);
+  FRIEND_TEST(HadoopAuthToLocalTest, sedRuleTest);
+  FRIEND_TEST(HadoopAuthToLocalTest, testRegexMatch);
+  FRIEND_TEST(HadoopAuthToLocalTest, threadSafeTest);
+  FRIEND_TEST(HadoopAuthToLocalTest, transformPrincipalTest);
+  FRIEND_TEST(HadoopAuthToLocalTest, negativeTransformPrincipalTest);
+
+  public:
+    //This constructor does not load rules, or set the default realm. Use init instead.
+    HadoopAuthToLocal();
+    //This should be the preferred way to initialize HadoopAuthToLocal
+    static std::unique_ptr<HadoopAuthToLocal> init(const std::string& filepath, krb5_context& ctx);
+    std::vector<std::string> getRules() const;
+    std::optional<std::string> matchPrincipalAgainstRules(std::string_view principal) const;
+};
+} // namespace security
+} // namespace kudu
