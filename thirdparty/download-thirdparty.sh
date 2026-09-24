ab2085ecbe866c0753a0c66cd79a56fc386628e7
Shrinidhi B <shrinidhib@acceldata.io>
2026-09-24 15:26:11 +0530

    ODP-7796: Add GCC 14 compatibility patch for LLVM 11
    LLVM 11.0.0 is missing #include <cstdint> for uintptr_t in
    Support/Signals.h. GCC 14 on RHEL10 has stricter header inclusion
    requirements than GCC 8.5 on RHEL8, causing build failures:
    
      error: 'uintptr_t' was not declared in this scope
        at include/llvm/Support/Signals.h:117
    
    Added patch to include <cstdint> header and incremented LLVM_PATCHLEVEL.

--- thirdparty/download-thirdparty.sh
+++ thirdparty/download-thirdparty.sh
@@ -332,7 +332,7 @@ fetch_and_patch \
  $PYTHON_SOURCE \
  $PYTHON_PATCHLEVEL
 
-LLVM_PATCHLEVEL=5
+LLVM_PATCHLEVEL=6
 fetch_and_patch \
  llvm-${LLVM_VERSION}-iwyu-${IWYU_VERSION}.src.tar.gz \
  $LLVM_SOURCE \
@@ -341,7 +341,8 @@ fetch_and_patch \
  "patch -d projects -p1 < $TP_DIR/patches/llvm-remove-cyclades-inclusion-in-sanitizer.patch" \
  "patch -p2 < $TP_DIR/patches/llvm-fix-missing-include.patch" \
  "patch -d projects -p1 < $TP_DIR/patches/llvm-Sanitizer-built-against-glibc-2_34-doesnt-work.patch" \
- "patch -d tools -p1 < $TP_DIR/patches/llvm-ignore-flto-values.patch"
+ "patch -d tools -p1 < $TP_DIR/patches/llvm-ignore-flto-values.patch" \
+ "patch -p1 < $TP_DIR/patches/llvm-gcc14-cstdint.patch"
 
 LZ4_PATCHLEVEL=0
 fetch_and_patch \
